1211defa8dedaa5378fd8f313ac0b9a8f7e0e487
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-10-28 20:40:33 -0700

    Initial Commit.

---
+++ src/components/BugItemCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ bugName }}</h1>
+    <img :src="image" :alt="imageName">
+  </div>
+</template>
+
+<script>
+import image from "../assets/cricket.jpeg";
+
+export default {
+  data: function () {
+    return {
+      image: image
+    }
+  },
+  name: 'HelloWorld',
+  props: {
+    bugName: String,
+    imageName: String,
+    imagePath: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
